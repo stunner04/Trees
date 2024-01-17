47b60f31e342e2660e8b807ba7970b3e5c6f4e3f
stunner04 <yashv0347@gmail.com>
2024-01-17 19:05:09 +0530

    Intial Commit

---
+++ buildBinaryTree.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int mod = 1e9 + 7;
+typedef long long ll;
+
+class TreeNode
+{
+public:
+    int data;
+    Node *left, right;
+
+    TreeNode(int root)
+    {
+        this->data = root;
+        left = right = nullptr;
+    }
+};
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
